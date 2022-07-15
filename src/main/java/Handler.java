1fc0dfde941116e4238e45dd95424d7f7d071548
Douglas Siqueira <siqueiradev@gmail.com>
2022-07-14 23:58:09 -0300

    teste fifo

--- src/main/java/Handler.java
+++ src/main/java/Handler.java
@@ -1,4 +1,5 @@
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -13,11 +14,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.UUID;
 
 public class Handler implements RequestHandler<S3EventNotification, Void> {
 
+    private LambdaLogger logger;
+
     @Override
     public Void handleRequest(S3EventNotification s3Event, Context context) {
+        this.logger = context.getLogger();
+
         AmazonS3 s3client = AmazonS3ClientBuilder
                 .standard()
                 .build();
@@ -26,7 +32,9 @@ public class Handler implements RequestHandler<S3EventNotification, Void> {
         S3Object object = s3client.getObject(new GetObjectRequest(s3.getBucket().getName(), s3.getObject().getKey()));
 
         try {
+            logger.log("inicio processamento");
             processFile(object.getObjectContent());
+            logger.log("fim processamento");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -36,21 +44,24 @@ public class Handler implements RequestHandler<S3EventNotification, Void> {
 
     private void processFile(InputStream objectData) throws IOException {
         final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+        logger.log("inicio envio");
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(objectData))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
                 sendQueue(sqs, line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        logger.log("fim envio");
         objectData.close();
     }
 
     private void sendQueue(AmazonSQS sqsConnection, String content) {
         SendMessageRequest send_msg_request = new SendMessageRequest()
-                .withQueueUrl("https://sqs.us-east-1.amazonaws.com/821845017640/filadeteste")
+                .withMessageGroupId("teste")
+                .withMessageDeduplicationId(UUID.randomUUID().toString())
+                .withQueueUrl("fifo")
                 .withMessageBody(content);
         sqsConnection.sendMessage(send_msg_request);
     }
